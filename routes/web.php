ee0b27d137d33f5d18a71e890327a010c1f0a213
Artsiom Osipchyk <Artsiom.Osipchyk@iacapps.com>
2020-03-20 03:14:55 +0300

    Prices add

--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,36 @@
 
 Route::get('/' , function () {
     return view('home');
-})->name('home');;
+})->name('home');
+
+Route::get('/from_moscow' , function () {
+    return view('from_moscow');
+})->name('fromMoscow');
+
+Route::get('/to_moscow' , function () {
+    return view('from_moscow');
+})->name('toMoscow');
+
+Route::get('/help_in_moscow' , function () {
+    return view('help_in_moscow');
+})->name('helpInMoscow');
+
+Route::get('/delivery_to_minsk' , function () {
+    return view('delivery_to_minsk');
+})->name('deliveryToMinsk');
+
+Route::get('/delivery_from_ikea' , function () {
+    return view('delivery_from_ikea');
+})->name('deliveryFromIKEA');
+
+Route::get('/prices' , function () {
+    return view('prices');
+})->name('prices');
+
+Route::get('/contacts' , function () {
+    return view('contacts');
+})->name('contacts');
+
+Route::get('/services' , function () {
+    return view('services');
+})->name('services');
