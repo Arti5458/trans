a7376987f8a0507871932fae039e1a41091ad0de
Artsiom Osipchyk <Artsiom.Osipchyk@iacapps.com>
2020-03-20 17:40:11 +0300

    Metric add

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -731,6 +731,21 @@
 {{--    </div><!-- .offcanvas-menu -->--}}
 </div><!-- /st-container -->
 
+<!-- Yandex.Metrika counter -->
+<script type="text/javascript" >
+    (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+        m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+    (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+    ym(61162459, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true,
+        webvisor:true
+    });
+</script>
+<noscript><div><img src="https://mc.yandex.ru/watch/61162459" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
 
 <!-- Preloader -->
 <div id="preloader">
